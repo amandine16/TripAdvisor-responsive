26e64c9e83cfb21efbf77c79cf73a27cc9e6c77e
Amandine <amandinedelavoie@live.fr>
2021-01-31 12:01:13 +0100

    version responsive pas terminée

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+const $ = document;
+
+$.addEventListener("DOMContentLoaded", () => {
+  console.log("document chargé !");
+  //Détection du click sur "envoie d'un email"
+  $.querySelector("#btn-connect").addEventListener("click", () => {
+    $.querySelector("#modal").classList.toggle("display");
+  });
+  $.querySelector(".close").addEventListener("click", () => {
+    $.querySelector("#modal").classList.remove("display");
+  });
+
+  //Ecoute de la soumission du formulaire d'envoie d'emails
+  $.querySelector("#formEmail").addEventListener("submit", async (event) => {
+    //Anule l'effet par défaut du submit
+    event.preventDefault();
+    console.log("button envoyer cliqué");
+    //Récupération des données du formulaire
+    const data = {
+      firstname: $.querySelector("#firstname").value,
+      lastname: $.querySelector("#lastname").value,
+      email: $.querySelector("#email").value,
+      subject: $.querySelector("#subject").value,
+      message: $.querySelector("#message").value,
+    };
+    //réponse du serveur
+    const response = await axios.post("http://localhost:3000/form", data);
+    console.log(response);
+    if (response.status === 200) {
+      alert("formulaire soumis");
+    } else {
+      alert("Erreur");
+    }
+  });
+});
